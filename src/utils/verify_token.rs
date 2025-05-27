4686e0b4abb3ce046d2fcbdbeaf2433713eaf7ea
saurabhwadekar <saurabhwadekar420@gmail.com>
2025-05-27 10:06:48 +0530

    use GLOBAL_TOKEN and update verify_token

--- src/utils/verify_token.rs
+++ src/utils/verify_token.rs
@@ -2,13 +2,22 @@ use super::get_env_vars::get_env_var;
 use reqwest;
 
 pub async fn verify_token(token: &str) -> bool {
+    let g_token = get_env_var("GLOBAL_TOKEN", "GLOBAL_TOKEN".to_string());
+
+    if get_env_var("USE_GLOBAL_TOKEN", false) {
+        return token == g_token;
+    }
+
     let client = reqwest::Client::new();
+    let secret = get_env_var("SERVER_SECRET", "SERVER_SECRET_KEY".to_string());
+    let verification_url = get_env_var(
+        "VERIFICATION_URL",
+        "http://localhost:3000/verify".to_string(),
+    );
+
     match client
-        .post(get_env_var(
-            "VERIFICATION_URL",
-            "http://localhost:3000/verify".to_string(),
-        ))
-        .json(&serde_json::json!({ "secret":get_env_var("SERVER_SECRET", "SERVER_SECRET_KEY".to_string()),"token": token }))
+        .post(verification_url)
+        .json(&serde_json::json!({ "secret":secret,"token": token }))
         .send()
         .await
     {
